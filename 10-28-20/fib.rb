3332847c7122b4cdcce1b19adf2667e05cbf8732
Alvin Zablan <azablan.dev@gmail.com>
2020-10-28 22:55:56 -0400

    rename notes and add fib in ruby

--- 10-28-20/fib.rb
+++ 10-28-20/fib.rb
@@ -1,8 +1,16 @@
-def foo(b = [])
-    p b.object_id
-end
-  
-foo()
-foo()
+# def fib(n)
+#   return 1 if n == 1 || n == 2
+#   return fib(n - 1) + fib(n - 2)
+# end
 
-# default args have different object ids
+def fib(n, memo={})
+  return memo[n] if memo.has_key?(n)
+  return 1 if n == 1 || n == 2
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo)
+  return memo[n]
+end
+
+p fib(6) # 8
+p fib(7) # 13
+p fib(8) # 21
+p fib(45) # 1134903170
