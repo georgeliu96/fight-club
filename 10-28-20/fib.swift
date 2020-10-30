dfc2beb5f37c30d4690900fa7db64c1cfabf6fb4
Alvin Zablan <azablan.dev@gmail.com>
2020-10-29 20:25:58 -0400

    fib in swift

---
+++ 10-28-20/fib.swift
@@ -0,0 +1,18 @@
+func fib(n: Int) -> Int {
+  var newMemo:[Int:Int] = [:]
+  return _fib(n: n, memo: &newMemo)
+}
+
+func _fib(n: Int, memo: inout Dictionary<Int, Int>) -> Int {
+  guard memo[n] == nil else { return memo[n] ?? -1 }
+  guard n > 2 else { return 1 }
+
+  memo[n] = _fib(n: n - 1, memo: &memo) + _fib(n: n - 2, memo: &memo)
+  return memo[n] ?? -1
+}
+
+print(fib(n: 6)) // 8
+print(fib(n: 7)) // 13
+print(fib(n: 8)) // 21
+print(fib(n: 9)) // 34
+print(fib(n: 42)) // 267914296
